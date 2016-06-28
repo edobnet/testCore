b0c45a1868107003eabe42119d41595bf60d63e5
edobnet <edobnet@163.com>
2016-06-28 18:02:28 +0800

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.Logging;
+using WebApplication.Data;
+using WebApplication.Models.EF;
 namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _db;
+         private readonly ILogger<HomeController> _logger;
+        public HomeController(ApplicationDbContext db,ILogger<HomeController> logger)
+        {
+            _db = db;
+            _logger =  logger;
+        }
         public IActionResult Index()
         {
+            _logger.LogInformation("ssssssssssss");
+           _db.Add(new ShopInfo()
+           {
+               Name="test111",
+           }.SetDefault());
+           _db.SaveChanges();
             return View();
         }
 
---
+++ Data/Migrations/20160628082520_MyFirstMigration.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication.Data.Migrations
+{
+    public partial class MyFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateTable(
+                name: "ShopInfo",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("Autoincrement", true),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(maxLength: 64, nullable: true),
+                    UpdateTime = table.Column<DateTime>(nullable: false),
+                    desc = table.Column<string>(maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShopInfo", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "ShopInfo");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName");
+        }
+    }
+}
